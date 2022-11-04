70f1231696e3ba032235a44a420563eedd066bba
Vladimir Ivancha <Vladimir.Ivancha@mail.ru>
2022-11-05 00:03:15 +0300

    feat: file structure - header, main, footer

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import "../../index.css";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
+
+function App() {
+  return (
+    <div className="page">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Hamburger from "../Hamburger/Hamburger"
+import Settings from "../Settings/Settings"
+
+function Header() {
+  return (
+    <>
+        <header className="header">
+        </header>
+        <Hamburger />
+        <Settings />
+    </>
+  );
+}
+export default Header;
